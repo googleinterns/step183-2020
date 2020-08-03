e8267c7eca3e21632981c0fd1d69ca44b1e886d4
Shreya Chatterjee <shreyachatte@google.com>
2020-08-03 21:29:21 +0000

    Formatting fixes

--- src/main/java/com/google/sps/servlets/generateServlet.java
+++ src/main/java/com/google/sps/servlets/generateServlet.java
@@ -14,20 +14,13 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
+import java.util.HashSet;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import com.google.gson.Gson;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
-import java.util.HashSet;
 
 /** Servlet that returns bucket list content */
 @WebServlet("/generate")
--- src/main/webapp/filterScript.js
+++ src/main/webapp/filterScript.js
@@ -1,30 +1,35 @@
-/* Turn a filter a different color when pressed, and change class to "unclicked-filter" or "clicked-filter" */
+/**
+* Turn a filter a different color when pressed, and change class accordingly 
+**/
 function turnBlueWhenClicked() {
-  let allFilters = document.querySelectorAll(".unclicked-filter");
+  const allFilters = document.querySelectorAll('.unclicked-filter');
   for (let i = 0; i < allFilters.length; i++) {
     allFilters[i].onclick = function() {
-      let currFilter = allFilters[i];
-      if (currFilter.classList.contains("unclicked-filter")) {
-        currFilter.classList.remove("unclicked-filter");
-        currFilter.classList.add("clicked-filter");
+      const currFilter = allFilters[i];
+      if (currFilter.classList.contains('unclicked-filter')) {
+        currFilter.classList.remove('unclicked-filter');
+        currFilter.classList.add('clicked-filter');
       } else {
-          currFilter.classList.remove("clicked-filter");
-          currFilter.classList.add("unclicked-filter");
+          currFilter.classList.remove('clicked-filter');
+          currFilter.classList.add('unclicked-filter');
       }
     }
   }
 } 
 
-/* Get all filters that have been clicked when user presses submit button, and pass array to servlet, and TODO: return success or error message. */
+/**
+* Get all filters that have been clicked when user presses submit button, 
+* and pass array to servlet, and TODO: return success or error message. 
+**/
 function getClickedFilters() {
-  let clickedArray = [];
-  let clickedFilters = document.querySelectorAll(".clicked-filter");
+  const clickedArray = [];
+  const clickedFilters = document.querySelectorAll('.clicked-filter');
   for (let i = 0; i < clickedFilters.length; i++) {
     console.log(i + ": " + clickedFilters[i].innerHTML);
     clickedArray[i] = clickedFilters[i].innerHTML;
   }
 
-  var JSONArray = JSON.stringify(clickedArray);
+  const JSONArray = JSON.stringify(clickedArray);
   console.log(JSONArray);
   fetch('/generate?clicked-array=' + JSONArray, {method: 'POST'}).then(response => response.json()).then((message) => {
       console.log(message);
