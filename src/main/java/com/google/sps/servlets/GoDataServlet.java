a56c15307e4d657b2e7e25d89025af76b427176d
Lily Zhou <lilymzhou@google.com>
2020-08-12 23:00:13 +0000

    Alphabetize imports in GoDataServlet.

--- src/main/java/com/google/sps/servlets/GoDataServlet.java
+++ src/main/java/com/google/sps/servlets/GoDataServlet.java
@@ -14,14 +14,14 @@
 
 package com.google.sps.servlets;
 
-import com.google.gson.Gson;
-import com.google.sps.data.ScavengerHunt;
-import java.io.IOException;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.gson.Gson;
+import com.google.sps.data.ScavengerHunt;
+import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
