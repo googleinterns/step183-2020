b6f0a75136c67a7cb6df60e96b7b86fa23a8a54f
Ryan Simone <ryansimone2324@gmail.com>
2020-09-02 19:48:10 +0000

    asserts that the value being stored in datastore is accurate

--- src/test/java/com/google/sps/DestinationDataServletTest.java
+++ src/test/java/com/google/sps/DestinationDataServletTest.java
@@ -1,18 +1,26 @@
 package com.google.sps;
 
-import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.gson.Gson;
+import com.google.sps.data.Destination;
+import com.google.sps.data.LatLng;
+import com.google.sps.data.Riddle;
 import com.google.sps.servlets.Constants;
 import com.google.sps.servlets.DestinationDataServlet;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
@@ -83,7 +91,49 @@ public final class DestinationDataServletTest {
     servlet.doPost(request, response);
 
     Assert.assertEquals(
-        1, datastore.prepare(new Query(Constants.DESTINATION_ENTITY)).countEntities(withLimit(10)));
+        1, datastore.prepare(new Query(Constants.DESTINATION_ENTITY)).countEntities());
+
+    Query query = new Query(Constants.DESTINATION_ENTITY);
+    PreparedQuery results = datastore.prepare(query);
+
+    ArrayList<Destination> destinations = new ArrayList();
+    for (Entity dest : results.asIterable()) {
+      Destination destination =
+          new Gson()
+              .fromJson((String) dest.getProperty(Constants.DESTINATION_JSON), Destination.class);
+      destinations.add(destination);
+    }
+
+    String actual = GSON.toJson(destinations.get(0));
+
+    LatLng location = new LatLng.Builder().withLat(123.456).withLng(234.567).build();
+ 
+    Riddle riddle =
+        new Riddle.Builder()
+            .withPuzzle("Stay away from me if you're afraid of heights")
+            .withHint("Overlooks the water")
+            .withHint("Golden-red in color")
+            .withHint("You have to pay to use me")
+            .build();
+    Destination.Obscurity level = Destination.Obscurity.EASY;
+    Set<Destination.Tag> tagEnums = new HashSet<Destination.Tag>();
+    tagEnums.add(Destination.Tag.HISTORICAL);
+    tagEnums.add(Destination.Tag.TOURIST);
+ 
+    Destination expectedDestination =
+        new Destination.Builder()
+            .withName("Golden Gate Bridge")
+            .withLocation(location)
+            .withCity("San Francisco")
+            .withDescription("Famous Bridge in SF")
+            .withRiddle(riddle)
+            .withTags(tagEnums)
+            .withObscurity(level)
+            .withPlaceId("123")
+            .build();
+    String expected = GSON.toJson(expectedDestination);
+
+    Assert.assertEquals(actual, expected);
 
     verify(response).sendRedirect(HOME_URL);
   }
